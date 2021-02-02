96d594e1fa45d0685372c1465467e8cc361298a2
Annie Zhou <wenyiz@uvic.ca>
2021-02-01 21:24:05 -0800

    Parse cookies and blacklist response codes.

--- SmartClient.py
+++ SmartClient.py
@@ -4,39 +4,95 @@ import time
 import os
 import select
 import threading
+import re
 from socket import *
 
 TLS_PORT = 443
 HTTP_PORT = 80
 PORT = 443
 BUFFER_SIZE = 2028
-cookies_str = []
+cookies_list = []
 
 https_support= 'No'
 http1_1_support = 'No'
 http2_support = 'No'
 
-def add_cookie_get_code(data):
-    global cookies_str
+COOKIE_NAME = 'cookie name: '
+EXPIRES_TIME = 'expires time: '
+DOMAIN_NAME = 'domain name: '
+
+def print_cookies(cookies_list):
+    for cookie in cookies_list:
+        if(COOKIE_NAME in cookie.keys()):
+            print(COOKIE_NAME, cookie[COOKIE_NAME], end = ', ')
+        if(EXPIRES_TIME in cookie.keys()):
+            print(EXPIRES_TIME, cookie[EXPIRES_TIME], end = ', ')
+        if(DOMAIN_NAME in cookie.keys()):
+            print(DOMAIN_NAME, cookie[DOMAIN_NAME], end = '')
+        print()
+    
+def parse_cookie(cookie):
+    cookie_dict = {}
+    cookie_data = cookie.split(';')
+    for item in cookie_data:
+        cookie_item = item.lower()
+        if('set-cookie:' in cookie_item):
+            stripped_cookie_key = item.split('=')
+            cookie_name_list = stripped_cookie_key[0].split(':')
+            cookie_name = cookie_name_list[1]
+            cookie_dict[COOKIE_NAME] = cookie_name
+        if('domain' in cookie_item):
+            domain_name_list = item.split('=')
+            domain_name = domain_name_list[1]
+            cookie_dict[DOMAIN_NAME] = domain_name
+        if('expires' in cookie_item):
+            expire_time_list = item.split('=')
+            expire_time = expire_time_list[1]
+            cookie_dict[EXPIRES_TIME] = expire_time
+    return cookie_dict
+
+def add_cookie(data):
+    global cookies_list
+
     #Split data by lines and store in list
     data_list = data.split('\n')
-    #Store response code
-    code = data_list[0]
     for item in data_list:
-        #Convert to lower case
-        item = item.lower()
+        cookie_item = item.lower()
         #Find cookies
-        if('set-cookie:' not in item):
+        if('set-cookie:' not in cookie_item):
             continue
-        cookies_str.append(item)
-    return code
+        cookies_list.append(parse_cookie(item))
 
-def test_http2_support(domain):
+def get_code(data):
+    data_list = data.split('\n')
+    return str(data_list[0])
+
+def format_result(result, code):
+    verdict = result + ", " + code
+    return verdict
+
+
+def interpret_response_code(code):
+    if(code == ""):
+        return 'No'
+
+    if(re.search(r'4\d\d', code)):
+        return format_result('No', code)
+    
+    if(re.search(r'5\d\d', code)):
+        return format_result('No', code)
+
+    if('HTTP/1.0' in code):
+        if('302' not in code):
+            return format_result('No', code)
+
+    return 'Yes'
+
+def test_http2_support(domain, request):
     global http2_support, http1_1_support
-    print("----HTTP2 SUPPORT ---")
-
+    print("----Testing HTTP2 Support---")
     #Create SSL Context
-    context = ssl.create_default_context()
+    context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
     #Set ALPN protocols
     context.set_alpn_protocols(['h2', 'http/1.1'])
     #Create connection
@@ -48,54 +104,54 @@ def test_http2_support(domain):
         conn.connect((domain, TLS_PORT))
     except ssl.SSLError as e:
         print("Probably invalid domain entered or can't find in DNS listing.")
+        return
     except Exception as e:
         print("Unexpected Error Occured: {}".format(e))
-    else:
-        print("connected...")
+        return
+        
     
-    #Send HTTP request
-    request = 'HEAD / HTTP/2\r\nHost: {}\r\n\r\n'.format(domain)
+    #Send HTTP2 request
+    print("----Request begin----")
+    print(request)
     try: 
         conn.send(request.encode())
     except Exception as e:
         print("Unexpected Error Occured: {}".format(e))
-    else:
-        print("requested...")
+        
 
+    print("----Request end-----\nHTTP request sent, awaiting response...\n\n")
     #Get selected alpn protocol
     protocol = conn.selected_alpn_protocol()
     #Print selected protocol
+    print("----Response output----")
     print("SELECTED PROTOCOL: " + repr(protocol).replace(' ', r'\s'))
  
     if(protocol == 'h2'):
         http2_support = 'Yes'
-        http1_1_support = 'Yes'
     elif(protocol == 'http/1.1'):
         http1_1_support = 'Yes'
     
-    #Get HTTP response 
+    #Get HTTP2 response 
     try:
-        data = conn.recv(BUFFER_SIZE).decode() 
+        data = conn.recv(BUFFER_SIZE).decode(errors='ignore') 
     except UnicodeDecodeError:
         print("Cannot decode message.")
     except Exception as e:
         print("Unexpected Error Occured: {}".format(e))
     else:
-        print("recieved...")
         print(data)
-        http2_support = http2_support + add_cookie_get_code(data)
+        add_cookie(data)
 
 
     conn.close() #Close connection
-    return False
 
 
-def test_https_support(domain):
-    global cookies_str
-    global https_support
-    print("----HTTPS SUPPORT ---", domain)
+def test_https_support(domain, request):
+    print("----Testing HTTPs Support---")
+    global cookies_list
+    global https_support, http1_1_support
     #Create SSL Context
-    context = ssl.create_default_context()
+    context = ssl.create_default_context(purpose=ssl.Purpose.CLIENT_AUTH)
     #Create connection
     sock = socket(AF_INET, SOCK_STREAM)
     #Wrap connection with SSL
@@ -105,40 +161,46 @@ def test_https_support(domain):
         conn.connect((domain, TLS_PORT))
     except ssl.SSLError as e:
         print("Probably invalid domain entered or can't find in DNS listing.")
+        return
     except Exception as e:
         print("Unexpected Error Occured: {}".format(e))
-    else:
-        print("connected...")
+        return
+        
     
     
-    #Send HTTP request
-    request = 'HEAD / HTTP/1.1\r\nHost: {}\r\n\r\n'.format(domain)
+    #Send HTTPs request
+    print("----Request begin---")
+    print(request)
     try: 
         conn.send(request.encode())
     except Exception as e:
         print("Unexpected Error Occured: {}".format(e))
-    else:
-        print("requested...")
+        
+
+    print("----Request end-----\nHTTPs request sent, awaiting response...\n\n")
 
     #Get HTTP response 
+    print("----Response output----")
     try:
-        data = conn.recv(BUFFER_SIZE).decode() 
+        data = conn.recv(BUFFER_SIZE).decode(errors='ignore') 
     except UnicodeDecodeError:
         print("Cannot decode message.")
     except Exception as e:
         print("Unexpected Error Occured: {}".format(e))
+        
     else:
-        print("recieved...")
         print(data)
-        https_support = add_cookie_get_code(data)
+        add_cookie(data)
+        response_code = get_code(data)
+        https_support = interpret_response_code(response_code)
+        http1_1_support = interpret_response_code(response_code)
     
     conn.close() #Close connection
-    return True
 
-def test_http1_1_support(domain):
+def test_http1_1_support(domain, request):
     global http1_1_support
-    print("----HTTP1.1 SUPPORT ---")
 
+    print("----Testing HTTP1.1 Support---")
     #Create connection
     conn = socket(AF_INET, SOCK_STREAM)
     #Open connection 
@@ -146,35 +208,40 @@ def test_http1_1_support(domain):
         conn.connect((domain, HTTP_PORT))
     except ssl.SSLError as e:
         print("Probably invalid domain entered or can't find in DNS listing.")
+        return
     except Exception as e:
         print("Unexpected Error Occured: {}".format(e))
-    else:
-        print("connected...")
+        return
+        
     
+
     #Send HTTP request
-    request = 'GET / HTTP/1.1\r\nHost: {}\r\n\r\n'.format(domain)
+    print("----Request begin---")
+    print(request)
     try: 
         conn.send(request.encode())
     except Exception as e:
         print("Unexpected Error Occured: {}".format(e))
-    else:
-        print("requested...")
+        
+    
+    print("----Request end-----\nHTTP request sent, awaiting response...\n\n")
 
     #Get HTTP response 
+    print("----Response output----")
     try:
-        data = conn.recv(BUFFER_SIZE).decode() 
+        data = conn.recv(BUFFER_SIZE).decode(errors='ignore') 
     except UnicodeDecodeError:
         print("Cannot decode message.")
     except Exception as e:
         print("Unexpected Error Occured: {}".format(e))
+        
     else:
-        print("recieved...")
         print(data)
-        http1_1_support = add_cookie_get_code(data)
-    
+        add_cookie(data)
+        response_code = get_code(data)
+        http1_1_support = interpret_response_code(response_code)
     
     conn.close() #Close connection
-    return True
 
 def main():
     #Read in input
@@ -183,17 +250,18 @@ def main():
         sys.exit(2)
     domain_name = str(sys.argv[1])
     try:
-        test_https_support(domain_name)
-        test_http1_1_support(domain_name)
-        test_http2_support(domain_name)
+        test_http2_support(domain_name, 'GET / HTTP/2\r\nHost: {}\r\nConnection: Keep-alive\r\n\r\n'.format(domain_name))
+        test_https_support(domain_name, 'GET / HTTP/1.1\r\nHost: {}\r\nConnection: Keep-alive\r\n\r\n'.format(domain_name))
+        test_http1_1_support(domain_name, 'GET / HTTP/1.1\r\nHost: {}\r\nConnection: Keep-alive\r\n\r\n'.format(domain_name))
     except Exception as e:
-        sys.exit(2)
-    
-    print("website: " + repr(domain_name).replace(' ', r'\s'))
-    print("1. Supports of HTTPs: " + https_support)
-    print("2. Supports of http1.1: " + http1_1_support) 
-    print("3. Supports of http2: " + http2_support) 
-    print("4: List of Cookies: \n", "\n".join(list(dict.fromkeys(cookies_str))))
+        print(e)
+    else:
+        print("\n\n\n\n\n\n\nwebsite: {}".format(domain_name))
+        print("1. Supports of HTTPs: {}".format(https_support))
+        print("2. Supports of http1.1: {}".format(http1_1_support)) 
+        print("3. Supports of http2: {}".format(http2_support)) 
+        print("4: List of Cookies: ")
+        print_cookies(cookies_list)
 
 if __name__ == "__main__":
     main()
